f3bfe5c3e77726e34d92a079094d1f4a9e3797a7
SaitoLuca <luca.saito@isitgo.it>
2023-06-01 19:12:15 +0200

    imported stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import logo from './logo.svg';
+//react stuff
+import React, { useRef, useState } from 'react';
 import './App.css';
+//firebase stuff
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
+//firebase hooks stuff
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 function App() {
   return (
